b607e6bcfc25703699a22cbe7394975573d0cbab
Jhalley Badua de Castro <joseph.halley@gmail.com>
2020-10-15 15:41:19 +0800

    Update jhalley.py

--- K-closest-points/jhalley.py
+++ K-closest-points/jhalley.py
@@ -37,3 +37,34 @@ class Solution(object):
                 heapq.heappop(h)
                 
         return [i[1] for i in h]
+    
+# Using quickSelect
+class Solution(object):
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        # Using quickSelect approach
+        calcDist = lambda i: i[0]**2 + i[1]**2
+        dist = [(calcDist(p), p) for p in points]
+        return [i[1] for i in self.quickSelect(dist, K)]
+    
+    def quickSelect(self, points, K):
+        if K <= 0:
+            return []
+        left, right = [], []
+        
+        mid = K/2
+        for i in range(len(points)):
+            if points[i][0] <= points[mid][0]:
+                left.append(points[i])
+            else:
+                right.append(points[i])
+        
+        if len(left) >= K:
+            return sorted(left, key=lambda i: i[0])[:K]
+        else:
+            return left + self.quickSelect(right, K-len(left))
+            
